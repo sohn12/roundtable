53d1c15b4be309dbdf02782c31af5e22f35a7561
sohn12 <sohan.reddy12@gmail.com>
2019-01-02 08:01:21 +0530

    Greet new user

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on("connection", function(socket) {
   console.log("new user connected");
 
+  socket.emit("newMessage", {
+    from: "Message",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("createMessage", message => {
     console.log("create Message", message);
     io.emit("newMessage", {
@@ -22,6 +34,11 @@ io.on("connection", function(socket) {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", function() {
